b8cf1db057093cbd9f4f2fdbdd20303536376b25
Victor Ighalo <victor.ighalo@centricgateway.com>
2021-09-13 11:52:23 +0100

    clean up

--- Plugin/CsrfValidatorSkip.php
+++ Plugin/CsrfValidatorSkip.php
@@ -3,19 +3,19 @@
 /**
  * Paystack Magento2 Module using \Magento\Payment\Model\Method\AbstractMethod
  * Copyright (C) 2019 Paystack.com
- * 
+ *
  * This file is part of Pstk/Paystack.
- * 
+ *
  * Pstk/Paystack is free software => you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http =>//www.gnu.org/licenses/>.
  */
@@ -40,10 +40,10 @@ class CsrfValidatorSkip {
         $request,
         $action
     ) {
-        if ("{$request->getModuleName()}/{$request->getActionName()}" == 'paystack/webhook') {
+        if ("{$request->getModuleName()}/{$request->getActionName()}" == 'seerbit/webhook') {
             return; // Skip CSRF check
         }
         $proceed($request, $action); // Proceed Magento 2 core functionalities
     }
-    
+
 }
